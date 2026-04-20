41a119ea3391902a08ba0b0b1cf431e1361b624e
ecarvalhogmail <ecarvalhoprg2012@gmail.com>
2026-04-19 22:19:41 -0300

    Aula06 implementada ideaProjects

--- src/main/java/edu/eduardocarvalho/StarWarsServices.java
+++ src/main/java/edu/eduardocarvalho/StarWarsServices.java
@@ -4,7 +4,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.time.temporal.ChronoUnit;
 
 @RegisterRestClient(baseUri = "https://swapi.info/api/")
 public interface StarWarsServices {
@@ -12,5 +17,19 @@ public interface StarWarsServices {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("starships")
+    @Timeout(value=3000L)
+    @Fallback(
+            fallbackMethod = ""
+    )
+    @CircuitBreaker(
+            requestVolumeThreshold = 2,
+            failureRatio = .5,
+            delay = 3000L,
+            successThreshold = 2
+    )
     public String getStarships();
+
+    default String getStarShipsFallback() {
+        return "Fallback";
+    }
 }
--- src/main/java/edu/eduardocarvalho/UniPDS_Resource.java
+++ src/main/java/edu/eduardocarvalho/UniPDS_Resource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.awt.*;
+import java.time.LocalDateTime;
 
 @Path("/unipds")
 @Produces(MediaType.TEXT_PLAIN)
@@ -16,14 +17,22 @@ public class UniPDS_Resource {
         return i;
     }
 
+    @GET
+    @Path("getIDiferentao")
+    public int getIDiferentao(){
+        return LocalDateTime.now().getNano();
+    }
+
     @POST
     public void addI(int i) {
         this.i++;
     }
+
     @DELETE
     public void removeI() {
         this.i--;
     }
+
     @PUT
     public void setI(int i) {
         this.i = i;
